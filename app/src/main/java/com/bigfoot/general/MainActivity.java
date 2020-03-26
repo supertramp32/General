498dec1e12d304d9bd4b16bce46a3f409402d705
Ashik <ashiksubedi32@gmail.com>
2020-03-26 10:19:01 +0545

    our team added

--- app/src/main/java/com/bigfoot/general/MainActivity.java
+++ app/src/main/java/com/bigfoot/general/MainActivity.java
@@ -123,6 +123,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        addOfferFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OurTeamActivity.class));
+
+            }
+        });
+
+
 
     }
 
---
+++ app/src/main/java/com/bigfoot/general/OurTeamActivity.java
@@ -0,0 +1,14 @@
+package com.bigfoot.general;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OurTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_team);
+    }
+}
